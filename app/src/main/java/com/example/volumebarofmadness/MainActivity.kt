2127b5bd440aa2640a9785dea20a13a513e37f6c
JungSangHyeon <dev.jusa@gmail.com>
2021-08-29 16:12:47 +0900

    Change Sound Finished

--- app/src/main/java/com/example/volumebarofmadness/MainActivity.kt
+++ app/src/main/java/com/example/volumebarofmadness/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.volumebarofmadness
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.AudioManager
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import androidx.appcompat.app.AppCompatActivity
 import com.example.volumebarofmadness.databinding.ActivityMainBinding
 import kotlin.math.abs
 import kotlin.math.tan
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +23,12 @@ class MainActivity : AppCompatActivity() {
 
         binding.frontVolum.drawable.level = 0
         binding.clickArea.setOnTouchListener(FireListener())
+
+
+
+
+
+
     }
 
     var level = 0
@@ -55,7 +64,7 @@ class MainActivity : AppCompatActivity() {
                     level+=100
                     Thread.sleep(10)
                 }
-            }catch(e:InterruptedException){
+            }catch (e: InterruptedException){
             }
         }
         chargingThread?.start()
@@ -68,10 +77,19 @@ class MainActivity : AppCompatActivity() {
                 runOnUiThread {
                     binding.ball.translationX = 0f
                     val x = (binding.bar.width*level/10000).toFloat() + (binding.bar.x - binding.ball.x) - binding.ball.width/2
-                    var rad = abs(maxAngle*level/10000) * Math.PI/360;
+                    var rad = abs(maxAngle * level / 10000) * Math.PI/360;
                     val y = (tan(rad) * x).toFloat()
                     FireAnimation.start(binding.ball, 500, x, y)
                 }
+
+                val audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
+                audioManager.setStreamVolume(
+                    AudioManager.STREAM_MUSIC,
+                    audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * level / 10000,
+                    AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE
+                )
+                Log.e("VOLUME", "vol: " + (audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * level / 10000))
+                Log.e("MAX VOLUME", "vol: " + (audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)))
 
                 while(level!=0){ // Reset Speaker & Wait Ball Fire Animation Finish
                     binding.frontVolum.drawable.level = level
@@ -80,7 +98,7 @@ class MainActivity : AppCompatActivity() {
                     level-=100
                     Thread.sleep(10)
                 }
-            }catch(e:InterruptedException){
+            }catch (e: InterruptedException){
             }
         }
         reloadingThread?.start()
