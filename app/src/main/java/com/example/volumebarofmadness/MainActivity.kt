5a41c8517d4fb714a5d49ffe99d72936558af8e8
JungSangHyeon <dev.jusa@gmail.com>
2021-08-29 13:46:54 +0900

    Ball Animation Finished

--- app/src/main/java/com/example/volumebarofmadness/MainActivity.kt
+++ app/src/main/java/com/example/volumebarofmadness/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.volumebarofmadness
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.Log
 import com.example.volumebarofmadness.databinding.ActivityMainBinding
 
@@ -16,20 +21,54 @@ class MainActivity : AppCompatActivity() {
 
         binding.frontVolum.drawable.level = 5000
 
-        var level = 0
-        var diff = 100
-        var maxAngle = -45
+//        var level = 0
+//        var diff = 100
+//        var maxAngle = -45
+//        Thread{
+//            while(true){
+//                binding.frontVolum.drawable.level = level
+//                binding.frontVolum.rotation = (maxAngle*level/10000).toFloat()
+//                binding.backVolum.rotation = (maxAngle*level/10000).toFloat()
+//                Log.d("ASD", ""+binding.frontVolum.rotation)
+//                level+=diff
+//                if(level==10000) diff=-100
+//                else if(level==0) diff=100
+//                Thread.sleep(10)
+//            }
+//        }.start()
+
         Thread{
             while(true){
-                binding.frontVolum.drawable.level = level
-                binding.frontVolum.rotation = (maxAngle*level/10000).toFloat()
-                binding.backVolum.rotation = (maxAngle*level/10000).toFloat()
-                Log.d("ASD", ""+binding.frontVolum.rotation)
-                level+=diff
-                if(level==10000) diff=-100
-                else if(level==0) diff=100
-                Thread.sleep(10)
+                runOnUiThread {
+                    ObjectAnimator.ofFloat(binding.ball, "translationX", 500f).apply {
+                        duration = 2000
+                        start()
+                    }.addListener(XFinishedListener())
+                    ObjectAnimator.ofFloat(binding.ball, "translationY", -100f).apply {
+                        duration = 1000
+                        start()
+                    }.addListener(YFinishedListener())
+                }
+                Thread.sleep(4000)
             }
         }.start()
+
+    }
+
+    inner class YFinishedListener() : AnimatorListenerAdapter() {
+        override fun onAnimationEnd(animation: Animator?) {
+            ObjectAnimator.ofFloat(binding.ball, "translationY", 0f).apply {
+                duration = 1000
+                start()
+            }
+        }
+    }
+    inner class XFinishedListener() : AnimatorListenerAdapter() {
+        override fun onAnimationEnd(animation: Animator?) {
+            ObjectAnimator.ofFloat(binding.ball, "translationX", 0f).apply {
+                duration = 2000
+                start()
+            }
+        }
     }
 }
