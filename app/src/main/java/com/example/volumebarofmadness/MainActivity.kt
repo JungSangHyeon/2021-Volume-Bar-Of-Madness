18b4c79d64cc031b1fd5fc01c1abf7dc2e63c164
JungSangHyeon <dev.jusa@gmail.com>
2021-08-29 21:10:55 +0900

    Refactoring

--- app/src/main/java/com/example/volumebarofmadness/MainActivity.kt
+++ app/src/main/java/com/example/volumebarofmadness/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.volumebarofmadness
 
 import android.media.AudioManager
 import android.os.Bundle
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
@@ -16,6 +15,16 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val maxAngle = -45
+    private val maxLevel = 10000
+    private val diffLevel = 100
+    private val levelChargingInterval = 10L // ms
+    private val ballMovingTime = 500L // ms
+
+    private var level = 0
+    private var chargingThread: Thread? = null
+    private var reloadingThread: Thread? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,37 +32,31 @@ class MainActivity : AppCompatActivity() {
 
         binding.frontVolum.drawable.level = 0
         binding.clickArea.setOnTouchListener(FireListener())
-
-        binding.bar.viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                binding.bar.viewTreeObserver.removeOnGlobalLayoutListener(this)
-                val audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
-                val nowVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
-
-                Log.e("INITIAL VOLUME", "vol : $nowVolume")
-                val x = (binding.bar.width*nowVolume/audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)).toFloat() + (binding.bar.x - binding.ball.x) - binding.ball.width/2
-                FireAnimation.start(binding.ball, 0, x, 0f)
-            }
-        })
-
+        binding.bar.viewTreeObserver.addOnGlobalLayoutListener(VolumeBarInitializeListener())
     }
 
-    var level = 0
-    var maxAngle = -45
-    var chargingThread: Thread? = null
-    var reloadingThread: Thread? = null
+    inner class VolumeBarInitializeListener : OnGlobalLayoutListener{
+        override fun onGlobalLayout() {
+            binding.bar.viewTreeObserver.removeOnGlobalLayoutListener(this)
+            val audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
+            val nowVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
 
-    inner class FireListener : View.OnTouchListener{
+            val x = (binding.bar.width*nowVolume/audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)).toFloat() + (binding.bar.x - binding.ball.x) - binding.ball.width/2
+            FireAnimation.start(binding.ball, 0, x, 0f)
+        }
+    }
+
+    inner class FireListener : View.OnTouchListener {
         override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                if(event!!.action == MotionEvent.ACTION_DOWN){
-                    reloadingThread?.interrupt()
-                    startCharging()
-                    return true
-                }else if (event!!.action == MotionEvent.ACTION_UP){
-                    chargingThread?.interrupt()
-                    startReloading()
-                    return true
-                }
+            if (event!!.action == MotionEvent.ACTION_DOWN) {
+                reloadingThread?.interrupt()
+                startCharging()
+                return true
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                chargingThread?.interrupt()
+                fireAndReload()
+                return true
+            }
             return false // no handle touch event
         }
     }
@@ -61,89 +64,45 @@ class MainActivity : AppCompatActivity() {
     private fun startCharging() {
         chargingThread = Thread{
             try {
-                while(level!=10000){
+                while(level!=maxLevel){
                     binding.frontVolum.drawable.level = level
-                    binding.frontVolum.rotation = (maxAngle*level/10000).toFloat()
-                    binding.backVolum.rotation = (maxAngle*level/10000).toFloat()
-                    level+=100
-                    Thread.sleep(10)
+                    binding.frontVolum.rotation = (maxAngle*level/maxLevel).toFloat()
+                    binding.backVolum.rotation = (maxAngle*level/maxLevel).toFloat()
+                    level+=diffLevel
+                    Thread.sleep(levelChargingInterval)
                 }
-            }catch (e: InterruptedException){
-            }
+            }catch (e: InterruptedException){}
         }
         chargingThread?.start()
     }
 
-    private fun startReloading() {
+    private fun fireAndReload() {
         reloadingThread = Thread{
             try {
-                Log.d("ASD", "Fire LEVEL : $level")
                 runOnUiThread {
                     binding.ball.translationX = 0f
-                    val x = (binding.bar.width*level/10000).toFloat() + (binding.bar.x - binding.ball.x) - binding.ball.width/2
-                    var rad = abs(maxAngle * level / 10000) * Math.PI/360;
+                    val x = (binding.bar.width*level/maxLevel).toFloat() + (binding.bar.x - binding.ball.x) - binding.ball.width/2
+                    val rad = abs(maxAngle * level / maxLevel) * Math.PI/360;
                     val y = (tan(rad) * x).toFloat()
-                    FireAnimation.start(binding.ball, 500, x, y)
+                    FireAnimation.start(binding.ball, ballMovingTime, x, y)
                 }
 
                 val audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
                 audioManager.setStreamVolume(
                     AudioManager.STREAM_MUSIC,
-                    audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * level / 10000,
+                    audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * level / maxLevel,
                     AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE
                 )
-                Log.e("VOLUME", "vol: " + (audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * level / 10000))
-                Log.e("MAX VOLUME", "vol: " + (audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)))
 
                 while(level!=0){ // Reset Speaker & Wait Ball Fire Animation Finish
                     binding.frontVolum.drawable.level = level
-                    binding.frontVolum.rotation = (maxAngle*level/10000).toFloat()
-                    binding.backVolum.rotation = (maxAngle*level/10000).toFloat()
-                    level-=100
-                    Thread.sleep(10)
+                    binding.frontVolum.rotation = (maxAngle*level/maxLevel).toFloat()
+                    binding.backVolum.rotation = (maxAngle*level/maxLevel).toFloat()
+                    level-=diffLevel
+                    Thread.sleep(levelChargingInterval)
                 }
-            }catch (e: InterruptedException){
-            }
+            }catch (e: InterruptedException){ }
         }
         reloadingThread?.start()
-    }
-
-    fun autoPlay(){
-        Thread {
-            while (true) {
-                var level = 0
-                var maxAngle = -45
-                for (i in 0..100 step (10)) {
-                    for (j in 0..i) {
-                        binding.frontVolum.drawable.level = level
-                        binding.frontVolum.rotation = (maxAngle * level / 10000).toFloat()
-                        binding.backVolum.rotation = (maxAngle * level / 10000).toFloat()
-                        level += 100
-                        Thread.sleep(10)
-                    }
-                    // Fire
-                    Log.d("ASD", "Fire LEVEL : $level")
-                    runOnUiThread {
-                        binding.ball.translationX = 0f
-                        val x =
-                            (binding.bar.width * level / 10000).toFloat() + (binding.bar.x - binding.ball.x) - binding.ball.width / 2
-                        var rad = abs(maxAngle * level / 10000) * Math.PI / 360;
-                        val y = (tan(rad) * x).toFloat()
-                        FireAnimation.start(binding.ball, 500, x, y)
-                    }
-
-                    // Reset
-                    for (j in 0..i) {
-                        binding.frontVolum.drawable.level = level
-                        binding.frontVolum.rotation = (maxAngle * level / 10000).toFloat()
-                        binding.backVolum.rotation = (maxAngle * level / 10000).toFloat()
-                        level -= 100
-                        Thread.sleep(10)
-                    }
-                    // Wait Fire Finish
-                    Thread.sleep(500)
-                }
-            }
-        }.start()
     }
 }
